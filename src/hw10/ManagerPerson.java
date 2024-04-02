329658357af066135c2b801e6056e241e591352c
OrliogloDenis <orlioglodenis24@gmail.com>
2024-04-02 20:49:15 +0300

    add hw10

---
+++ src/hw10/ManagerPerson.java
@@ -0,0 +1,8 @@
+package hw10;
+
+public class ManagerPerson {
+    public static void main(String[] args) {
+        hw10.Person person=new hw10.Person("Denis",30,240);
+        person.printPersonInfo();
+    }
+}
---
+++ src/hw10/Person.java
@@ -0,0 +1,55 @@
+package hw10;
+
+
+class Person{
+    String name;
+    public int age;
+    public int height;
+
+    public void printPersonInfo(){
+        System.out.println(getName());
+        System.out.println(getAge());
+        System.out.println(getHeight());
+    }
+    public void setName(String name){
+        if(name.length()>3&&name.length()<15){
+            this.name=name;
+        }else{
+            System.out.println("большое имя!исправьте");
+        }
+    }
+    public void setAge(int age) {
+        if (age > 0 && age < 100) {
+            this.age = age;
+        } else {
+            System.out.println("Возраст должен быть больше 0 и меньше 100.");
+        }
+    }
+    public void setHeight(int height){
+        if(height>=120 &&height<=220){
+            this.height=height;
+        }else{
+            System.out.println("рост не коректный,исправьте");
+        }
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Person(String name,int age,int height){
+        setName(name);
+        setAge(age);
+        setHeight(height);
+    }
+
+
+}
+
